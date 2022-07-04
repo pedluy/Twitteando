6b9b1e825fa6a0e6b10b21f1afa51e7c1ebcd25a
pedro hidalgo <pedluy@gmail.com>
2022-07-04 13:18:58 +0200

    Método comprobando login

---
+++ bd/intentologin.go
@@ -0,0 +1,25 @@
+package bd
+
+import (
+	"github.com/pedluy/twitteando/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+/*IntentoLogin devuelve un bool de si es ok o no y si es Ok te trae el modelo del usuario*/
+func IntentoLogin(email string, password string) (models.Usuario, bool, string) {
+	/* Creamos una variable tipo model. Como vemos no pongo BD antes del paquete porque estamos dentro del mismo paquete.*/
+	usu, encontrado, id := ChequeoYaExisteUsuario(email)
+	if encontrado == false {
+		return usu, false, id
+	}
+	/* Ahora compruebo que la clave es igual a la de la DB encriptada*/
+	/* Creo una variable tipo slice byte*/
+	passwordBytes := []byte(password)
+	passwordBD := []byte(usu.Password)
+	/*ahora uso una funcion de bcrypt que permita comparar dos password primero mando la encryptada (hash)*/
+	err := bcrypt.CompareHashAndPassword(passwordBD, passwordBytes)
+	if err != nil {
+		return usu, false, id
+	}
+	return usu, true, id
+}
